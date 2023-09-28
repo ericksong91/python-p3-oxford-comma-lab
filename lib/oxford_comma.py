ed7ab9c9c5a50486915523a717822548ba31ff97
Eric Song <ericksong91@gmail.com>
2023-09-28 17:55:02 -0400

    Finished assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,15 @@
 def oxford_comma(items):
-    return None
+    new_list = []
+
+    if len(items) > 1:
+        if len(items) == 2:
+            return " and ".join(items)
+        for item in items:
+            if items[len(items) - 1] == item:
+                new_list.append(f"and {item}")
+            else:
+                new_list.append(f"{item}, ")
+    else:
+        print("".join(items))
+        return "".join(items)
+    return "".join(new_list)
